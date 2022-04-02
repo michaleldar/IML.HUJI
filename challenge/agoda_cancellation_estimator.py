8f117f7ff4c47cdf6c03fcbe2c8a937cbc286c95
michaleldar <101139814+michaleldar@users.noreply.github.com>
2022-04-02 23:06:47 +0300

    Add files via upload

--- challenge/agoda_cancellation_estimator.py
+++ challenge/agoda_cancellation_estimator.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
 from typing import NoReturn
+
+import sklearn.linear_model
+from sklearn.linear_model import LogisticRegression
+
 from IMLearn.base import BaseEstimator
 import numpy as np
+import pandas as pd
+import datetime
+import math
 
 
 class AgodaCancellationEstimator(BaseEstimator):
@@ -14,6 +21,7 @@ class AgodaCancellationEstimator(BaseEstimator):
         Instantiate an estimator for solving the Agoda Cancellation challenge
 
         Parameters
+
         ----------
 
 
@@ -21,6 +29,8 @@ class AgodaCancellationEstimator(BaseEstimator):
         ----------
 
         """
+        self.estimator_ = LogisticRegression()
+        self.fitted_ = False
         super().__init__()
 
     def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
@@ -39,7 +49,85 @@ class AgodaCancellationEstimator(BaseEstimator):
         -----
 
         """
-        pass
+        data = AgodaCancellationEstimator.adjust_data(X)
+        y_adjusted = AgodaCancellationEstimator.adjust_response(y)
+        self.estimator_.fit(data, y_adjusted)
+        self.fitted_ = True
+
+    @staticmethod
+    def adjust_response(X):
+        y = np.zeros(X.shape[0])
+        for sample in range(X.shape[0]):
+            if isinstance(X[sample], str):
+                y[sample] = 1
+            else:
+                y[sample] = 0
+        return y
+
+    @staticmethod
+    def adjust_data(X):
+        # days between booking and check in
+        col_1 = AgodaCancellationEstimator.date_to_days(X[:, 0]) - AgodaCancellationEstimator.date_to_days(X[:, 1])
+        # days between checkout and check in
+        col_2 = AgodaCancellationEstimator.date_to_days(X[:, 2]) - AgodaCancellationEstimator.date_to_days(X[:, 0])
+        # payment method(now/later)
+        col_3 = AgodaCancellationEstimator.pay_Now_or_later(X[:, 3])
+        # is first booking
+        col_4 = AgodaCancellationEstimator.first_booking(X[:, 4])
+
+        # not sure but made better score
+        # col_5 = X[:, 5]
+
+        #cancellation policy
+        col_6 = AgodaCancellationEstimator.canellation_policy(X[:, 6])
+
+        col_7 = AgodaCancellationEstimator.first_booking(X[:, 7])
+        # number of children
+        # col_5 = X[:, 5]
+        # number of adults
+        #col_5 = X[:, 5]
+        return np.array([col_1, col_2, col_3, col_4, col_6, col_7]).T
+
+    @staticmethod
+    def first_booking(X):
+        false_scalar, true_scalar = 0, 1
+        y = np.zeros(X.shape[0])
+        for sample in range(X.shape[0]):
+            if X[sample]:
+                y[sample] = true_scalar
+            else:
+                y[sample] = false_scalar
+        return y
+
+    @staticmethod
+    def canellation_policy(X):
+        y = np.zeros(X.shape[0])
+        for sample in range(X.shape[0]):
+            if "100P" not in X[sample]:
+                y[sample] = 100
+            else:
+                y[sample] = -1
+        return y
+
+    @staticmethod
+    def pay_Now_or_later(X):
+        now_scalar, later_scalar = -100, 100
+        y = np.zeros(X.shape[0])
+        for sample in range(X.shape[0]):
+            if X[sample] == "Pay Now":
+                y[sample] = now_scalar
+            else:
+                y[sample] = later_scalar
+        return y
+
+    @staticmethod
+    def date_to_days(X):
+        def s_to_int(s):
+            splited_by_space = s.split()
+            splited_by_backslash = splited_by_space[0].split("-")
+            return int(splited_by_backslash[0]) + int(splited_by_backslash[1]) * 30 + int(splited_by_backslash[2]) * 365
+
+        return np.array([s_to_int(s) for s in X])
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -55,7 +143,7 @@ class AgodaCancellationEstimator(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        return np.zeros(X.shape[0])
+        return self.estimator_.predict(AgodaCancellationEstimator.adjust_data(X))
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -74,4 +162,5 @@ class AgodaCancellationEstimator(BaseEstimator):
         loss : float
             Performance under loss function
         """
-        pass
+        return self.estimator_.score(AgodaCancellationEstimator.adjust_data(X),
+                                     AgodaCancellationEstimator.adjust_response(y))
--- challenge/agoda_cancellation_prediction.py
+++ challenge/agoda_cancellation_prediction.py
@@ -3,11 +3,14 @@ from IMLearn.utils import split_train_test
 
 import numpy as np
 import pandas as pd
+from IMLearn.base import BaseEstimator
+from sklearn.model_selection import train_test_split
 
 
 def load_data(filename: str):
     """
     Load Agoda booking cancellation dataset
+
     Parameters
     ----------
     filename: str
@@ -21,18 +24,36 @@ def load_data(filename: str):
     3) Tuple of ndarray of shape (n_samples, n_features) and ndarray of shape (n_samples,)
     """
     # TODO - replace below code with any desired preprocessing
-    full_data = pd.read_csv(filename).dropna().drop_duplicates()
-    features = full_data[["h_booking_id",
-                          "hotel_id",
-                          "accommadation_type_name",
-                          "hotel_star_rating",
-                          "customer_nationality"]]
+    full_data = pd.read_csv(filename).drop_duplicates()
+    features = full_data[["checkin_date",
+                          "booking_datetime",
+                          "checkout_date",
+                          "charge_option",
+                          "is_first_booking",
+                          "original_selling_amount",
+                          "cancellation_policy_code",
+                          "is_first_booking"
+                          ]]
     labels = full_data["cancellation_datetime"]
 
     return features, labels
 
 
-def evaluate_and_export(estimator: BaseEstimator, X: np.ndarray, filename: str):
+def load_test_set(file_name: str):
+    full_data = pd.read_csv(file_name).drop_duplicates()
+    features = full_data[["checkin_date",
+                          "booking_datetime",
+                          "checkout_date",
+                          "charge_option",
+                          "is_first_booking",
+                          "original_selling_amount",
+                          "cancellation_policy_code",
+                          "is_first_booking"
+                          ]]
+    return features
+
+
+def evaluate_and_export(estimator: BaseEstimator, X: np.ndarray, real_values, filename: str):
     """
     Export to specified file the prediction results of given estimator on given testset.
 
@@ -51,18 +72,24 @@ def evaluate_and_export(estimator: BaseEstimator, X: np.ndarray, filename: str):
         path to store file at
 
     """
-    pd.DataFrame(estimator.predict(X), columns=["predicted_values"]).to_csv(filename, index=False)
+    pd.DataFrame({"predictrd_Values": estimator.predict(X), "real_values": real_values}).to_csv(filename, index=False)
+
+
+def predict_test_set(estimator: BaseEstimator, X: np.ndarray, file_name: str):
+    pd.DataFrame({"predictrd_Values": estimator.predict(X)}).to_csv(file_name, index=False)
 
 
 if __name__ == '__main__':
-    np.random.seed(0)
-
     # Load data
-    df, cancellation_labels = load_data("../datasets/agoda_cancellation_train.csv")
-    train_X, train_y, test_X, test_y = split_train_test(df, cancellation_labels)
-
+    data, cancellation_labels = load_data("../datasets/agoda_cancellation_train.csv")
+    train_X, test_X, train_y, test_y = split_train_test(data, cancellation_labels)
     # Fit model over data
-    estimator = AgodaCancellationEstimator().fit(train_X, train_y)
+    estimator = AgodaCancellationEstimator().fit(np.array(train_X), np.array(train_y))
+    test_set = load_test_set("test_set_week_1.csv")
 
     # Store model predictions over test set
-    evaluate_and_export(estimator, test_X, "id1_id2_id3.csv")
+    evaluate_and_export(estimator, np.array(test_X), AgodaCancellationEstimator.adjust_response(test_y),
+                        "check_prediction.csv")
+    predict_test_set(estimator, np.array(test_set),"316080076_313598492_318456290.csv")
+    print("score is:")
+    print(estimator.loss(np.array(test_X), np.array(test_y)))
